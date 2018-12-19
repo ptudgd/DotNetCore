fae4207f2222ec0350f5295aaccc0c15880937cf
Nguyễn Hoàng Duy <nguyen.hoang.duy1997@gmail.com>
2018-12-19 15:47:20 +0700

    Fix Content.cs

--- Web.MVC/Startup.cs
+++ Web.MVC/Startup.cs
@@ -16,17 +16,7 @@ namespace Web.MVC
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/build";
-            });
-        }
+       
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
