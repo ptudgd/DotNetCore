21d4c3c2ccb43056703426c7c233166eacfedf23
Lê Tuấn Anh <letuananh30@gmail.com>
2018-12-20 16:51:15 +0700

    add controller admin

---
+++ Web.MVC/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Web.MVC.Models;
+
+namespace Web.MVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+                return View();
+            
+        }
+        
+    }
+}
---
+++ Web.MVC/Controllers/AdminHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Web.MVC.Models;
+
+namespace Web.MVC.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();          
+        }
+
+    }
+}
